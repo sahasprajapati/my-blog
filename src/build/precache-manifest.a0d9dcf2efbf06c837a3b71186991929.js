8177da02562afb444d363f12696e7c7399cce886
Sahas Prajapati <b10prajapati@gmail.com>
2020-05-29 23:49:15 +0545

    Initital Commit

---
+++ src/build/precache-manifest.a0d9dcf2efbf06c837a3b71186991929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42b2b4592a9544904044dac3f2318a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "056f82e13f3bce8750f8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b24c0248e4cc554c66b",
+    "url": "/static/js/2.99650adb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.99650adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056f82e13f3bce8750f8",
+    "url": "/static/js/main.9bc0a8b1.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
